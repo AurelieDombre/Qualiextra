641ac0d8f0eb8c1eeb8518fc80ead90fa2bc1476
AurelieDombre <au.dombre@gmail.com>
2022-10-12 11:31:06 +0200

    indentation

--- src/Controller/User/PackageController.php
+++ src/Controller/User/PackageController.php
@@ -11,8 +11,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-
-
 class PackageController extends AbstractController
 {   
     /**
